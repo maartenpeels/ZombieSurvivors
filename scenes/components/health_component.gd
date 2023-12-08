2e293f0eeca1e4d7f59b36c0a5884b41537dac48
Maarten Peels <maartenpeels@gmail.com>
2023-12-08 15:06:17 +0100

    Components

---
+++ scenes/components/health_component.gd
@@ -0,0 +1,31 @@
+extends Node
+class_name HealthComponent
+
+signal died
+signal health_changed
+
+@export var max_health: float = 10
+var current_health: float
+
+
+func _ready():
+	current_health = max_health
+
+
+func damage(damage_amount: float):
+	current_health = max(0, current_health - damage_amount)
+	health_changed.emit()
+	Callable(check_death).call_deferred()
+
+
+func get_health_percent():
+	if max_health <= 0:
+		return 0
+	
+	return min(current_health / max_health, 1)
+
+
+func check_death():
+	if current_health == 0:
+		died.emit()
+		owner.queue_free()

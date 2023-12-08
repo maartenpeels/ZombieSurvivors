c5f2d30947d3183d53c832bb18371225c14f6bfc
Maarten Peels <maartenpeels@gmail.com>
2023-12-08 15:04:51 +0100

    Basic enemy

---
+++ scenes/game_object/basic_enemy/basic_enemy.gd
@@ -0,0 +1,24 @@
+extends CharacterBody2D
+
+const MAX_SPEED = 40
+
+@onready var health_component: HealthComponent = $HealthComponent
+@onready var visuals: Node2D = $Visuals
+
+
+func _process(delta):
+	var direction = get_direction_to_player()
+	
+	velocity = direction * MAX_SPEED
+	move_and_slide()
+	
+	var move_sign = sign(velocity.x)
+	if move_sign != 0:
+		visuals.scale = Vector2(move_sign, 1)
+
+
+func get_direction_to_player():
+	var player_node = get_tree().get_first_node_in_group("player") as Node2D
+	if player_node != null:
+		return (player_node.global_position - global_position).normalized()
+	return Vector2.ZERO
---
+++ scenes/game_object/basic_enemy/vial_drop_component.gd
@@ -0,0 +1,27 @@
+extends Node
+
+@export_range(0, 1) var drop_percent: float = 0.5
+@export var health_component: HealthComponent
+@export var vial_scene: PackedScene
+
+
+func _ready():
+	health_component.died.connect(on_died)
+
+
+func on_died():
+	if randf() > drop_percent:
+		return
+	
+	if vial_scene == null:
+		return
+	
+	if not owner is Node2D:
+		return
+	
+	var spawn_position = (owner as Node2D).global_position
+	var vial_instance = vial_scene.instantiate() as Node2D
+	
+	vial_instance.global_position = spawn_position
+	var entities_layer = get_tree().get_first_node_in_group("entities_layer")
+	entities_layer.add_child(vial_instance)
